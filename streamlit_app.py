be689267ea925bfbb72fe92838a951934cdbdf70
VAH102985 <99217889+VAH102985@users.noreply.github.com>
2023-02-14 16:13:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
+
 streamlit.title("My parents New Healthy Diner")
+
+streamlit.header("Breakfast")
+
+
+streamlit.text("Omega 3 & blueberry oatmeal")
+streamlit.text("Kale, Spinach & Rock smoothie")
+streamlit.text("Hard-boiled free-range eggs")
